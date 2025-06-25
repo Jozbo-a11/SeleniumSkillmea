588c082dd7a13707da2287ff88b4ef325a2942cc
Jozbo-a11 <jozef.boko@gmail.com>
2025-06-25 19:56:35 +0200

    init commit

---
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+
+public class SelTestTutorialPointCheck {
+
+    private WebDriver ovladac;
+
+    @BeforeEach
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adulik\\IdeaProjects\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
+
+
+
+    }
+
+}
